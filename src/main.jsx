3c542510075eae7990b4fa20287563711e3fa852
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2024-06-12 00:46:43 +0600

    added routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "../src/assets/css/main.css";
 import { Provider } from "react-redux";
 import store from "./redux/store/store.js";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import CounterPage from "./pages/CounterPage.jsx";
+import TodoPage from "./pages/TodoPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <CounterPage />,
+  },
+  {
+    path: "/counter",
+    element: <CounterPage />,
+  },
+  {
+    path: "/todo",
+    element: <TodoPage />,
+  },
+  {
+    path: "/*",
+    element: (
+      <div className="flex justify-center items-center h-screen text-3xl">
+        404 NOT FOUND!
+      </div>
+    ),
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
